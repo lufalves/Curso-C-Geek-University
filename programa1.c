c8e3e51e707cd04db8427988770b49e841bf4bc3
Luiz Alves <lufalves23@hotmail.com>
2020-02-07 15:52:29 -0300

    Programa 1. Entrada, Saida e Variaveis

---
+++ programa1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+	//declarando variáveis
+	int idade;
+
+	//Função para escrever a saída padrao (monitor)
+	printf ("Qual sua idade? ");
+
+	//Receber dados
+	scanf ("%d", &idade);
+
+	//Saída
+	printf ("Sua idade é %d", idade);
+
+
+	return 0;
+} //chave delimita um bloco
